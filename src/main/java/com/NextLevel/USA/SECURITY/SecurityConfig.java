d265b7d8074e148b94574cf3c8b4dcfe2384e009
sufiyan820 <148753310+sufiyan820@users.noreply.github.com>
2026-03-07 17:08:44 +0530

    updates for devops *

--- src/main/java/com/NextLevel/USA/SECURITY/SecurityConfig.java
+++ src/main/java/com/NextLevel/USA/SECURITY/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/admins/register").permitAll()
                         .requestMatchers("/api/auth/login").permitAll()
-                        .requestMatchers("/api/products/**").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers("/api/products/**").permitAll()
                         .requestMatchers("/api/users/register").permitAll()
                         .requestMatchers("/api/cart/**").hasAnyRole("ADMIN", "USER")
                         .requestMatchers("/api/orders/**").hasAnyRole("ADMIN", "USER")
